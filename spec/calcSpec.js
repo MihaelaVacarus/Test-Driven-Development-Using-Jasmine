a85d326a923fe2792b3205d606c503c27b0c0f66
MihaelaVacarus <mihaela.vacarus@outlook.com>
2020-11-27 10:11:28 +0000

    Add defensive programming for error in addition

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -6,6 +6,9 @@ describe ("Calculator", function () {
         it("should return 25", function() {
             expect(addition(7, 19)).toBe(26);
         })
+        it("should return an error if we don't supply two numbers", function() {
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        })
     });
 });
 
